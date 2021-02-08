2dd15e24f37d8e9473aeac225ef50eeb8df56270
vinit-poojary-au8 <vinitpoojary00@gmail.com>
2021-02-08 14:15:58 +0530

    error

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -4,33 +4,39 @@ import { Card } from 'react-bootstrap'
 import Rating from './Rating'
 
 const Product = ({ product }) => {
-  return (
-    <Card className='my-3 p-3 rounded'>
-      <Link to={`/product/${product._id}`}>
-        <Card.Img src={product.image} variant='top' />
-      </Link>
+	return (
+		<Card className='my-3 p-3 rounded'>
+			<Link to={`/product/${product._id}`}>
+				<Card.Img
+					src={product.image}
+					variant='top'
+					style={{ width: 300, height: 'auto' }}
+				/>
+			</Link>
 
-      <Card.Body>
-        <Link to={`/product/${product._id}`}>
-          <Card.Title as='div'>
-            <strong>{product.name}</strong>
-          </Card.Title>
-        </Link>
+			<Card.Body>
+				<Link to={`/product/${product._id}`}>
+					<Card.Title as='div'>
+						<strong>{product.name}</strong>
+					</Card.Title>
+				</Link>
 
-        <Card.Text as='div'>
-          <Rating
-            value={product.rating}
-            text={`${product.numReviews} reviews`}
-          />
-        </Card.Text>
+				<Card.Text as='div'>
+					<Rating
+						value={product.rating}
+						text={`${product.numReviews} reviews`}
+					/>
+				</Card.Text>
 
-        <Card.Text as='h3'>₹{product.price}</Card.Text>
-        <Card.Text as='h6'>
-          {product.countInStock > 0 ? 'In Stock' : 'Out Of Stock'}
-        </Card.Text>
-      </Card.Body>
-    </Card>
-  )
+				<Card.Text as='h3'>₹{product.price}</Card.Text>
+				<Card.Text as='h6'>
+					{product.countInStock > 0
+						? 'In Stock'
+						: 'Out Of Stock'}
+				</Card.Text>
+			</Card.Body>
+		</Card>
+	)
 }
 
 export default Product
